ca25dcd30a1c407e31cb46302e22e6c300ca1dc3
Shurui Zhang <Shurui.Zhang@student.tafesa.edu.au>
2022-04-05 23:16:11 +0930

    Changed ticket price

--- Ticketing/Ticketing/TicketPrice.cs
+++ Ticketing/Ticketing/TicketPrice.cs
@@ -13,10 +13,10 @@ namespace Ticketing
         private decimal amountDue;
         private decimal mPrice;
 
-        const decimal mdecBalcony = 35.5m;
-        const decimal mdecGeneral = 28.75m;
-        const decimal mdecBox = 62.0m;
-        const decimal mdecDiscount = 5.0m; 
+        const decimal mdecBalcony = 55.5m;
+        const decimal mdecGeneral = 58.75m;
+        const decimal mdecBox = 92.0m;
+        const decimal mdecDiscount = 15.0m; 
 
         private int Section
         {
